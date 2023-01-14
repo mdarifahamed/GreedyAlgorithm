b1e56d5f2e6707b48504077bde31561e47526f13
Arif <arifonly4@gmail.com>
2023-01-14 20:01:37 +0530

    GreedyAlgo

--- PairsMinAbs.java
+++ PairsMinAbs.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class PairsMinAbs {
     public static void main(String[] args) {
-        int A[] = {1, 2, 3};
-        int B[] = {2, 1, 3};
+        // int A[] = {1, 2, 3};
+        // int B[] = {2, 1, 3};
+
+        int A[] = { 4, 1, 8, 7 };
+        int B[] = { 2, 3, 6, 5 };
 
         Arrays.sort(A);
         Arrays.sort(B);
